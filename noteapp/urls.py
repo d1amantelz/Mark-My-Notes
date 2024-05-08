6c51a9aa8da129cba61ea12a1b3eb04091c38bee
d1amantelz <diamantelzvadim@gmail.com>
2024-05-08 19:19:32 +0400

    Minor changes

--- noteapp/urls.py
+++ noteapp/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path('notes/<int:note_id>/view_mode/', NoteViewMode.as_view(), name='note_view_mode'),
     path('notes/<int:note_id>/update_note/', NoteInfoUpdateView.as_view(), name='update_note'),
     path('notes/by_category/<int:category_id>', NotesByCategoryView.as_view(), name='notes_by_category'),
-    path('notes/delete/<int:note_id>/', delete_note, name='delete_note'),
-    path('notes/export/<int:note_id>/', export_note, name='export_note'),
+    path('notes/<int:note_id>/delete/', delete_note, name='delete_note'),
+    path('notes/<int:note_id>/export/', export_note, name='export_note'),
     path('notes/search/', note_search, name='note_search'),
 
     # CATEGORY VIEWS
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('categories/create/', CategoryCreateView.as_view(), name='create_category'),
     path('categories/<int:category_id>/update_category/', CategoryUpdateView.as_view(), name='update_category'),
-    path('categories/delete/<int:category_id>/', delete_category, name='delete_category'),
+    path('categories/<int:category_id>/delete/', delete_category, name='delete_category'),
 
     # PROFILE VIEWS
     path('profile/', ProfileUpdateView.as_view(), name='profile'),
