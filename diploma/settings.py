9bfac52bf58e75e74a5ac0cfeeee02f98c84c3be
d1amantelz <diamantelzvadim@gmail.com>
2024-05-29 13:21:26 +0400

    Returned internal fonts folder

--- diploma/settings.py
+++ diploma/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
 GOOGLE_FONTS_DIR = BASE_DIR / "fonts"
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "fonts"]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
